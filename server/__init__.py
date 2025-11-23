3b1ddbad0cffcb7181871b5ee7c2064da4744884
Alexandr <proshka20081010@gmail.com>
2025-11-23 10:20:18 +0300

    Update __init__.py

--- server/__init__.py
+++ server/__init__.py
@@ -86,6 +86,10 @@ app = FastAPI(
 
 class SecurityHeadersMiddleware:
     async def __call__(self, scope, receive, send):
+
+        if scope["type"] == "websocket":
+            return await self.app(scope, receive, send)
+
         async def send_wrapper(message):
             if message["type"] == "http.response.start":
                 headers = dict(message.get("headers", []))
